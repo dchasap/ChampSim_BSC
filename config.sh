be4462cc7ecc366cf5bebc5e08630488ca8c19a7
Dimitrios Chasapis <dchasapi@sert-entry-3.ac.upc.es>
2024-09-04 10:01:49 +0200

    Fixed Make

--- config.sh
+++ config.sh
@@ -29,6 +29,7 @@ def parse_file(fname):
         return json.load(rfp)
 
 if __name__ == '__main__':
+    print("staring main")
     champsim_root = os.path.dirname(os.path.abspath(__file__))
     test_root = os.path.join(champsim_root, 'test')
     parser = argparse.ArgumentParser(description='Configure ChampSim')
